7b0a5d1682b03c1e0d71813c93fcf111d79a8a16
smartquailprod <smartquail.stage@gmail.com>
2023-02-18 19:51:22 -0500

    Update celery.py

--- qnd12_app_stg/qnd12_app_stg/celery.py
+++ qnd12_app_stg/qnd12_app_stg/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd12_app_stg.settings.local')
 
-app = Celery('qnd12_app_dev')
+app = Celery('qnd12_app_stg')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
